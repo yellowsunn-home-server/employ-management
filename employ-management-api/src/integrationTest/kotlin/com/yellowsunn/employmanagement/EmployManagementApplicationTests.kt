5b5e6f271e03960bc925cd1de6d180d5a9432d76
yellowsunn <43487002+yellowsunn@users.noreply.github.com>
2023-11-19 03:35:36 +0900

    [TSK-22] employ-management-api 모듈 추가 (#1)

---
+++ employ-management-api/src/integrationTest/kotlin/com/yellowsunn/employmanagement/EmployManagementApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.yellowsunn.employmanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmployManagementApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
